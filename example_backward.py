898efe8accca9894d2deb791a9b27d89559b14cd
JP <jpvigneaux@gmail.com>
2024-08-15 15:54:01 -0700

    file with relevance tracing

---
+++ example_backward.py
@@ -0,0 +1,74 @@
+
+import torch
+from transformers import AutoTokenizer
+from lxt.models.llama import LlamaForCausalLM, attnlrp
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = "cuda:1"
+model_path = "/home/achtibat/PythonProjects/crp-for-multimodal/LLama/Llama-2-7b-chat-hf"
+
+def save_heatmap(values, tokens, figsize, title, save_path):
+    fig, ax = plt.subplots(figsize=figsize)  # Increase the size of the figure
+
+    abs_max = abs(values).max()
+    im = ax.imshow(values, cmap='bwr', vmin=-abs_max, vmax=abs_max)
+    
+    layers = np.arange(values.shape[-1])
+
+    ax.set_xticks(np.arange(len(layers)))
+    ax.set_yticks(np.arange(len(tokens)))
+
+    ax.set_xticklabels(layers)
+    ax.set_yticklabels(tokens)
+
+    plt.title(title)
+    plt.xlabel('Layers')
+    plt.ylabel('Tokens')
+    plt.colorbar(im)
+
+    plt.show()
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
+
+def hidden_relevance_hook(module, input, output):
+    if isinstance(output, tuple):
+        output = output[0]
+    module.hidden_relevance = output.detach().cpu()
+
+
+if __name__ == "__main__":
+
+    # load model & apply AttnLRP
+    model = LlamaForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16, device_map=device)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    model.eval()
+    attnlrp.register(model)
+
+    # apply hooks
+    for layer in model.model.layers:
+        layer.register_full_backward_hook(hidden_relevance_hook)
+
+    # forward & backard pass
+    prompt_response = f"<s>I have 5 cats and 3 dogs. How many cats do I have again? Ah, I have "
+    input_ids = tokenizer(prompt_response, return_tensors="pt", add_special_tokens=False).input_ids.to(device)
+    input_embeds = model.get_input_embeddings()(input_ids)
+
+    output_logits = model(inputs_embeds=input_embeds.requires_grad_(), use_cache=False).logits
+    max_logits, max_indices = torch.max(output_logits[:, -1, :], dim=-1)
+    max_logits.backward(max_logits)
+
+    print("Prediction:", tokenizer.convert_ids_to_tokens(max_indices))
+
+    # trace relevance through layers
+    relevance_trace = []
+    for i in range(0, 32):
+        relevance = model.model.layers[i].hidden_relevance[0].sum(-1)
+        relevance = relevance / relevance.abs().max()
+        relevance_trace.append(relevance)
+
+    relevance_trace = torch.stack(relevance_trace)
+
+    tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
+    save_heatmap(relevance_trace.float().numpy().T, tokens, (20, 10), f"Latent Relevance Trace (Normalized)", f'latent_rel_trace.png')
+
+
